80bb4af6cc7e24347b288cec60382714fcb7c168
Paulo Cesar <paulocgsantos99@gmail.com>
2024-09-25 17:41:20 -0300

    feat: add payment success page

--- frontend/src/pages/shopping-view/payment-success.jsx
+++ frontend/src/pages/shopping-view/payment-success.jsx
@@ -1,10 +1,20 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { useNavigate } from "react-router-dom";
 
 const ShoppingPaymentSuccess = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      
-    </div>
-  )
+    <Card className="p-10">
+      <CardHeader className="p-0">
+        <CardTitle className="text-4xl">Pagamento efetuado com sucesso!</CardTitle>
+      </CardHeader>
+      <Button className="mt-5" onClick={() => navigate("/shop/account")}>
+        Vizualizar pedido
+      </Button>
+    </Card>
+  );
 }
 
 export default ShoppingPaymentSuccess
